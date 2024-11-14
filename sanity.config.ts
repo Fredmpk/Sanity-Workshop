1bb170d1b9766b52dc1fe2a59886b1665bb8ac8a
Fred <frederik.koenig@posteo.de>
2024-11-14 11:52:59 +0100

    reboot

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,8 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {structure} from './schemaTypes/strucure'
+import {defaultDocumentNode} from './schemaTypes/strucure/defaultDocumentNode'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +12,7 @@ export default defineConfig({
   projectId: 'qbz0fr70',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool({structure, defaultDocumentNode}), visionTool()],
 
   schema: {
     types: schemaTypes,
